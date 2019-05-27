41ce26681f5d0182389cf64ba7bc5258dff48a36
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-05-27 14:52:41 -0500

    screenshot

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BlockBlister
 
+#Screenshot 
+![screenshot](https://github.com/jeressia/BlockBlister/blob/master/assets/blockblister.png?raw=true)
+
+![screenshot](https://github.com/jeressia/BlockBlister/blob/master/assets/blockblister.png?raw=true)
--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -2,6 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const authDiv = document.getElementById('auth');
+const moviesDiv = document.getElementById('movies');
+const movieTitle = document.getElementById('movie-title');
 const authNavbar = document.getElementById('navbar-button-auth');
 const searchNavbar = document.getElementById('search-box-nav');
 const logoutNavbar = document.getElementById('navbar-button-logout');
@@ -9,10 +11,13 @@ const moviesNavbar = document.getElementById('navbar-button-movies');
 const starsNavbar = document.getElementById('navbar-button-stars');
 const popularNavbar = document.getElementById('navbar-button-popular');
 
+
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       authDiv.classList.add('hide');
+      moviesDiv.classList.remove('hide');
+      movieTitle.classList.remove('hide');
       authNavbar.classList.add('hide');
       logoutNavbar.classList.remove('hide');
       searchNavbar.classList.remove('hide');
@@ -21,6 +26,8 @@ const checkLoginStatus = () => {
       popularNavbar.classList.remove('hide');
     } else {
       authDiv.classList.remove('hide');
+      moviesDiv.classList.add('hide');
+      movieTitle.classList.add('hide');
       authNavbar.classList.remove('hide');
       logoutNavbar.classList.add('hide');
       searchNavbar.classList.add('hide');
