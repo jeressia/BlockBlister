ad19a071e6c3d44588e9bed95fdcdb25226a317b
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-05-29 20:53:13 -0500

    user can add moves

---
+++ src/javascripts/components/addmovie/addmovie.js
@@ -0,0 +1,51 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import addmovieData from '../../helpers/data/addmovieData';
+import util from '../../helpers/util';
+
+const movieDiv = document.getElementById('movies');
+const newMovieDiv = document.getElementById('addmovie');
+
+const createNewMovie = (e) => {
+  e.preventDefault();
+  const newMovie = {
+    title: document.getElementById('title').value,
+    genre: document.getElementById('genre').value,
+    id: firebase.auth().currentUser.uid,
+    filmRating: document.getElementById('filmRating').value,
+    releaseYear: document.getElementById('rYear').value,
+    description: document.getElementById('description').value,
+    imageURL: document.getElementById('imageURL').value,
+  };
+  addmovieData.addNewMovie(newMovie)
+    .then(() => {
+      document.getElementById('title').value = '';
+      document.getElementById('genre').value = '';
+      document.getElementById('filmRating').value = '';
+      document.getElementById('rYear').value = '';
+      document.getElementById('description').value = '';
+      document.getElementById('imageURL').value = '';
+      movieDiv.classList.remove('hide');
+      newMovieDiv.classList.add('hide');
+    })
+    .catch(err => console.error('no new friends', err));
+  console.error(newMovie);
+};
+
+const newMovieButton = () => {
+  document.getElementById('add-movie-btn').classList.add('hide');
+  document.getElementById('hideTitle').classList.remove('hide');
+  movieDiv.classList.add('hide');
+  newMovieDiv.classList.remove('hide');
+  document.getElementById('saveNewMovie').addEventListener('click', createNewMovie);
+};
+
+const showMovies = () => {
+  let domString = '<h2 id="hideTitle" class="hide">Add New Movie</h2>';
+  domString += '<button id="add-movie-btn" class="btn btn-info">Add Movie</button>';
+  util.printToDom('addme', domString);
+  document.getElementById('add-movie-btn').addEventListener('click', newMovieButton);
+};
+
+export default { showMovies };
--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -3,29 +3,40 @@ import util from '../../helpers/util';
 
 import './movies.scss';
 
-let movies = [];
+// let movies = [];
 
-const domStringBuilder = () => {
-  let domString = '';
-  movies.forEach((movie) => {
-    domString += '<div class="wrapper">';
-    domString += `<div class="rating-card-top">RATED: ${movie.filmRating}</div>`;
-    domString += `<div id="${movie.id}" class="card movie" style="width: 18rem;">`;
-    domString += `<img class="card-img-top" src=${movie.imageURL} alt="${movie.name}">`;
-    domString += '</div>';
-    domString += '</div>';
-  });
-  util.printToDom('movies', domString);
-};
+// const domStringBuilder = () => {
+//   let domString = '';
+//   movies.forEach((movie) => {
+//     domString += '<div class="wrapper">';
+//     domString += `<div class="rating-card-top">RATED: ${movie.filmRating}</div>`;
+//     domString += `<div id="${movie.id}" class="card movie" style="width: 18rem;">`;
+//     domString += `<img class="card-img-top" src=${movie.imageURL} alt="${movie.name}">`;
+//     domString += '</div>';
+//     domString += '</div>';
+//   });
+//   util.printToDom('movies', domString);
+// };
 
 const initializeMovies = () => {
   moviesData.getMoviesData()
     .then((resp) => {
       const moviesResults = resp.data.movies;
-      movies = moviesResults;
-      domStringBuilder();
+      // movies = moviesResults;
+      // domStringBuilder();
+      const movieValues = Object.values(moviesResults);
+      let domString = '';
+      movieValues.forEach((movie) => {
+        domString += '<div class="wrapper">';
+        domString += `<div class="rating-card-top">RATED: ${movie.filmRating}</div>`;
+        domString += `<div id="${movie.id}" class="card movie" style="width: 18rem;">`;
+        domString += `<img class="card-img-top" src=${movie.imageURL} alt="${movie.name}">`;
+        domString += '</div>';
+        domString += '</div>';
+      });
+      util.printToDom('movies', domString);
     })
     .catch(err => console.error(err));
 };
 
-export default { initializeMovies, domStringBuilder };
+export default { initializeMovies };
